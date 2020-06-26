6283dff57c73127a49f93e0c3690cf67245788b7
meaghan jennings <meaghanljennings@gmail.com>
2020-06-26 21:06:14 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,8 @@
 def introduction(name)
   name = "Josh"
 puts "Hi, my name is #{name}."
-end 
+end
+
 def introduction_with_language(language)
   language = "Ember.js"
 puts "Hi, my name is #{name} and I am learning to program in #{language}."
